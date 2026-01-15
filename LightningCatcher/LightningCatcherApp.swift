d42ae5c6b0e9542addcf36ccea4a335016394262
Tianren <13001238706@163.com>
2026-01-16 00:30:13 +0800

    Initial Commit

---
+++ LightningCatcher/LightningCatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightningCatcherApp.swift
+//  LightningCatcher
+//
+//  Created by 王天任 on 1/16/26.
+//
+
+import SwiftUI
+
+@main
+struct LightningCatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
